3ef9741a0cc5adf0135a9edfd0faf1c1bb32fc97
tediko <tediko123@wp.pl>
2021-05-08 15:51:36 +0200

    Add: Collapse menu class

---
+++ src/js/collapseMenu.js
@@ -0,0 +1,75 @@
+export default class CollapseMenu {
+    constructor() {
+        if (!this.vars()) return false;
+        this.setupEvents();
+    }
+    
+    vars() {
+        this.selectors = {
+            button: 'data-expand',
+            results: 'data-results-wrapper',
+            openClass: 'open',
+            closeClass: 'close'
+        };
+
+        this.button = document.querySelector(`[${this.selectors.button}]`);
+        this.results = document.querySelector(`[${this.selectors.results}]`);
+        
+        if (!this.button || !this.results) return false;
+
+        this.expanded = this.button.getAttribute('aria-expanded') === 'false' ? false : true;
+        this.open = false;
+        this.timer = false;
+        return true;
+    }
+
+    // Hamburger event listener
+    setupEvents() {
+        this.button.addEventListener('click', () => this.toggle());
+    }
+
+    // Toggle hamburger menu
+    toggle() {
+        !this.open ? this.show() : this.hide();
+    }
+
+    // Animation while hamburger is open
+    show() {
+        this.button.style.pointerEvents = "none";
+
+        this.results.classList.add('close');
+
+        this.expanded = !this.expanded;
+        this.button.setAttribute('aria-expanded', this.expanded);
+        this.open = true;
+        
+        this.timer = window.setTimeout(() => {
+            this.button.style.pointerEvents = "all";
+            this.timer = false;
+        }, 250);
+    }
+
+    // Animation while hamburger is closed
+    hide() {
+        this.button.style.pointerEvents = "none";
+
+        this.results.classList.add('open');
+        
+        this.expanded = !this.expanded;
+        this.button.setAttribute('aria-expanded', this.expanded);
+
+        this.timer = window.setTimeout(() => {
+            this.results.classList.remove('open');
+            this.results.classList.remove('close');
+            this.button.style.pointerEvents = "all";
+            this.timer = false;
+        }, 500);
+        this.open = false;
+    }
+
+    disable() {
+        this.results.classList.remove('open');
+        this.results.classList.remove('close');
+        this.button.style.pointerEvents = "all";
+    }
+}
